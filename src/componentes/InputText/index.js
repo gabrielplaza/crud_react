5d4f83b197b1592d47bbf3985d903599161bad1d
gabriel.plaza <gabriel.plaza@lansolver.com>
2023-06-22 16:44:24 -0300

    Criado os Primeiros Componentes

---
+++ src/componentes/InputText/index.js
@@ -0,0 +1,18 @@
+import './InputText.css'
+
+const InputText = () => {
+    return (
+        
+    
+    <div class="group">      
+    <input type="text" required/>
+    <span class="highlight"></span>
+    <span class="bar"></span>
+    <label>Name</label>
+  </div>
+    
+  
+    )
+}
+
+export default InputText
